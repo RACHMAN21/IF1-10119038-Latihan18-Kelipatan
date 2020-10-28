bc7af8fc0af1328ad23b5bed38062015dbff1284
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-28 11:59:03 +0700

    Upload Project

---
+++ src/main/java/if1/latihan18/kelipatan/Kelipatan.java
@@ -0,0 +1,21 @@
+package if1.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF-1
+ * NIM   : 10119038
+ * Deskripsi Program : Program ini berisi untuk menampilkan bilangan kelipatan
+ *                     3.5 s/d 35.
+ */
+
+public class Kelipatan {
+    
+    public static void main(String[] args) {
+        double i;
+        for(i=0;i<=35;i+=3.5) {
+            System.out.println(i);
+        }
+    }
+}
